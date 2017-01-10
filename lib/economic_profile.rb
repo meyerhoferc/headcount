4bffcf5eeca0d4d24c598cc760e0819ec1e9d591
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-10 12:57:26 -0700

    adds median household income methods

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,2 +1,25 @@
+require 'pry'
+require_relative 'unknown_data_error'
 class EconomicProfile
-end 
+  attr_reader :identifier
+  def initialize(identifier)
+    @identifier = identifier
+  end
+
+  def median_household_income_in_year(year)
+    keys = @identifier[:median_household_income].keys
+    raise(UnknownDataError) unless keys.any? { |key| key.include?(year) }
+    keys = keys.reject { |key| !key.include?(year) }
+    @identifier[:median_household_income][keys.first]
+  end
+
+  def median_household_income_average
+    years = @identifier[:median_household_income].keys.flatten
+    incomes = years.map { |year| median_household_income_in_year(year) }
+    find_average(incomes)
+  end
+
+  def find_average(data)
+    (data.reduce(:+) / data.count).to_i
+  end
+end
--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -19,7 +19,6 @@ class EconomicProfileTest < Minitest::Test
   end
 
   def test_it_has_an_identifier_hash_and_has_keys
-    skip 
     assert_equal Hash, ep.identifier.class
     keys = [:median_household_income, :children_in_poverty,
       :free_or_reduced_price_lunch, :title_i, :name]
@@ -31,29 +30,39 @@ class EconomicProfileTest < Minitest::Test
     keys = [:median_household_income, :children_in_poverty,
       :free_or_reduced_price_lunch, :title_i, :name]
     keys.each do |key|
+      years = ep.identifier[key]
+
+    end
+  end
+
+  def test_has_no_repeats_of_values_inside_of_one_keys_hash
+    skip
+    keys = [:median_household_income, :children_in_poverty,
+      :free_or_reduced_price_lunch, :title_i, :name]
+    keys.each do |key|
       values =  ep.identifier[key].values
       tally = values.count do |value|
         value == value
+        binding.pry
       end
-      assert_includes([0. 1], tally)
+      assert_includes([0, 1], tally)
     end
-    # no idea if this will work, use different enum
   end
 
   def test_returns_median_household_income
-    skip
     actual_1 = ep.median_household_income_in_year(2005)
     actual_2 = ep.median_household_income_in_year(2014)
-    actual_3 = ep.median_household_income_in_year(2075)
     assert_equal 50000, actual_1
     assert_equal 60000, actual_2
-    assert_raises(UnknownDataError) { actual_3 }
+    assert_equal Fixnum, actual_1.class
+    assert_equal Fixnum, actual_2.class
+    assert_raises(UnknownDataError) { ep.median_household_income_in_year(2075) }
   end
 
   def test_returns_average_median_household_income
-    skip
     actual = ep.median_household_income_average
     assert_equal 55000, actual
+    assert_equal Fixnum, actual.class
   end
 
   def test_returns_percentage_of_children_in_poverty
